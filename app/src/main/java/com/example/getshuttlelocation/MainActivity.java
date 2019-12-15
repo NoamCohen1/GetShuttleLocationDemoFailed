4ac8f063914ae58a754413a5c5d9f82b0cf68cce
Noam Cohen <noam.cohen4@live.biu.ac.il>
2019-12-15 20:25:42 +0200

    nice try.

--- app/src/main/java/com/example/getshuttlelocation/MainActivity.java
+++ app/src/main/java/com/example/getshuttlelocation/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.getshuttlelocation;
 
+import android.Manifest;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.Task;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermission();
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        System.out.println(latitude);
+        System.out.println(longitude);
+//        Task<Location> location = client.getLastLocation();
+//        System.out.println("STEMI");
+//        System.out.println(location.getResult().getLatitude() + ", " + location.getResult().getLongitude());
+
+    }
+
+    private void checkPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
     }
 }
