1092cbc6b32a061da45682c3fbbd9fcf82ea0a61
Noam Cohen <noam.cohen4@live.biu.ac.il>
2020-03-20 14:20:06 +0200

    OMG2

--- app/src/main/java/com/example/getshuttlelocation/MainActivity.java
+++ app/src/main/java/com/example/getshuttlelocation/MainActivity.java
@@ -85,20 +85,20 @@ public class MainActivity extends AppCompatActivity {
 
         LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         android.location.Location location = null;
-//        while (true) {
-//            //System.out.println(lm.GPS_PROVIDER);
-//            location = lm.getLastKnownLocation(lm.GPS_PROVIDER);
-//            //Location l = location.getResult();
-//            String msg = "New Latitude1: " + location.getLatitude() + " New Longitude1: " + location.getLongitude();
-//            System.out.println(msg);
-//
-//            Context context = getApplicationContext();
-//            CharSequence text = msg;
-//            int duration = Toast.LENGTH_SHORT;
-//
-//            Toast toast = Toast.makeText(context, text, duration);
-//            toast.show();
-//        }
+        while (true) {
+            //System.out.println(lm.GPS_PROVIDER);
+            location = lm.getLastKnownLocation(lm.GPS_PROVIDER);
+            //Location l = location.getResult();
+            String msg = "New Latitude1: " + location.getLatitude() + " New Longitude1: " + location.getLongitude();
+            System.out.println(msg);
+
+            Context context = getApplicationContext();
+            CharSequence text = msg;
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
 //        Criteria criteria = new Criteria();
 //        criteria.setAccuracy(Criteria.ACCURACY_FINE);
 //
@@ -133,13 +133,15 @@ public class MainActivity extends AppCompatActivity {
 //        String msg = "New Latitude1: " + location.getLatitude() + " New Longitude1: " + location.getLongitude();
 //        System.out.println(msg);
 //        lm.requestLocationUpdates("gps", 0, 0, locationListener);
-        for (String provider : lm.getProviders(true)) {
-            System.out.println(provider);
-            location = lm.getLastKnownLocation(provider);
-            String msg = "New Latitude1: " + location.getLatitude() + " New Longitude1: " + location.getLongitude();
-            System.out.println(msg);
-            lm.requestLocationUpdates(provider, 0, 0, locationListener);
-        }
+        // ????????????
+//        for (String provider : lm.getProviders(true)) {
+//            System.out.println(provider);
+//            location = lm.getLastKnownLocation(provider);
+//            String msg = "New Latitude1: " + location.getLatitude() + " New Longitude1: " + location.getLongitude();
+//            System.out.println(msg);
+//            lm.requestLocationUpdates(provider, 0, 0, locationListener);
+//        }
+        // ????????????
         //3 seconds and 10 meters
         //Location loc = getLastKnownLocation(LocationManager.GPS_PROVIDER);
         //lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
